b64d70bc74e19588eafeb2751fbf259b58f85b68
unknown <CIT173E-24@M303.uc-bcf.edu.ph>
2023-10-05 17:30:43 +0800

    Practice

---
+++ PHPprojects/codeact.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Code Activity Practice</title>
+</head>
+<body>
+    <h1>Codevility</h1>
+    <?php
+        echo "Forda code ang mga ferzon<br>";
+        echo "Oneshot Capstone, AMEN!<br>";
+        echo "Practice<br>";
+
+        $int_var = 100;
+        $a = 100;
+        $b = 95;
+
+        $c = $a + $b;
+        echo "Addition: $c <br>";
+        
+        $c = $a - $b;
+        echo "Subtraction: $c";
+
+
+    ?>
+</body>
+</html>
